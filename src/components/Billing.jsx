df48f4aa28105cfa35e2cdf9ebf91913821a9f08
David Burgos <88894919+dburgos26@users.noreply.github.com>
2024-11-03 19:21:29 -0500

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -25,7 +25,7 @@ export default function Billing() {
     if (phoneJson === null) {
         phoneJson = {
             id: 1,
-            name: "Samsung Galaxy S24",
+            name: "Samsung Galaxy S25",
             brand: "Samsung",
             price_per_day: 10,
             availability: 10,
